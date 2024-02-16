c212cb5489227379101a5a16962d30394131babe
felixyuri25 <ysantos@xcapitalbank.com.br>
2024-02-16 12:41:46 -0300

    Editando as variaveis de ambiente

--- mobile/src/api/index.ts
+++ mobile/src/api/index.ts
@@ -2,7 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.100.160:3000",
+  // baseURL: "http://seuIp rodando o backend ou localhost:3000",
 });
 
 api.interceptors.request.use(
